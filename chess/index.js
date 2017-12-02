76524486e437accf5d3069bec6e509e095154c8f
Evan Burak <evanburak@gmail.com>
2017-12-02 18:25:52 -0500

    Change piece enum to be strings

--- chess/index.js
+++ chess/index.js
@@ -1,8 +1,8 @@
 export const PIECE = {
-  PAWN: 0,
-  KNIGHT: 1,
-  BISHOP: 2,
-  ROOK: 3,
-  QUEEN: 4,
-  KING: 5,
+  PAWN: 'PAWN',
+  KNIGHT: 'KNIGHT',
+  BISHOP: 'BISHOP',
+  ROOK: 'ROOK',
+  QUEEN: 'QUEEN',
+  KING: 'KING',
 }
