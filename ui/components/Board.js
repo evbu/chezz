ff6ce7e57ace553d0790c33312db3745bef1d105
Evan Burak <evanburak@gmail.com>
2017-12-02 13:33:34 -0500

    Add Board component

---
+++ ui/components/Board.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const SIZE = 8
+const SQUARES = SIZE * SIZE
+
+const isOdd = (num) => num % 2
+const isEven = (num) => !isOdd(num)
+
+export const Board = () => {
+  let tiles = []
+
+  for (let i = 0; i < SQUARES; i++) {
+    const row = Math.floor(i / SIZE)
+    const col = i % SIZE
+
+    tiles.push(
+      <div key={i} style={{
+        'background': (isOdd(row) && isEven(col)) || (isEven(row) && isOdd(col)) ? 'white' : 'black',
+        'width': 45,
+        'height': 45,
+        'float': 'right',
+      }} />
+    )
+  }
+
+  return (
+    <div style={{
+      'width': 45 * SIZE,
+      'height': 45 * SIZE,
+    }}>
+      {tiles}
+    </div>
+  )
+}
